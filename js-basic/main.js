ec157a97c0580c89a12a59c5154dd93a95f1b573
Babubu157 <mgloooo@yahoo.com>
2025-02-18 12:53:30 +0800

    init

---
+++ js-basic/main.js
@@ -0,0 +1,19 @@
+console.log("Сайн уу JS");
+
+let message = "hello" ; //string
+console.log(message);
+
+let age = 42; //number
+console.log(age);
+
+let gpa = 4.5; //number
+console.log(gpa);
+
+let isStudent = true; //boolean
+console.log(isStudent); 
+
+let x; //undefined
+console.log(x);
+
+let n = null; //null
+console.log(n);
