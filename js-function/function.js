b8787a0a7dd09bd54740191202729a45d029c555
babubu157 <gboloro@gmail.com>
2025-03-12 14:32:14 +0800

    tsagaan sar bodlogo

--- js-function/function.js
+++ js-function/function.js
@@ -90,5 +90,103 @@ console.log(sum);
  const add1 = (a,b) => a + b;
 
 
+<<<<<<< Updated upstream
 let urDun = add(50,60);
 console.log("Arrow add function: ", urDun)ax
+=======
+let f = div(20, 10);
+console.log("/", f);
+
+console.log("===================== Bituunii bodlogo ===================");
+
+const employees = [
+  { id: 1, name: "Бат", age: 25, position: "Developer", salary: 2500000 },
+  { id: 2, name: "Сараа", age: 30, position: "Manager", salary: 3500000 },
+  { id: 3, name: "Дорж", age: 22, position: "Intern", salary: 1000000 },
+  { id: 4, name: "Тэмүүлэн", age: 40, position: "CEO", salary: 6000000 },
+  { id: 5, name: "Одмаа", age: 28, position: "Designer", salary: 2700000 },
+  { id: 6, name: "Ганбаа", age: 35, position: "HR", salary: 2200000 },
+  { id: 7, name: "Мөнхөө", age: 29, position: "QA Engineer", salary: 2400000 },
+  {
+    id: 8,
+    name: "Номин",
+    age: 33,
+    position: "Project Manager",
+    salary: 3700000,
+  },
+  { id: 9, name: "Хишигээ", age: 24, position: "Marketing", salary: 2300000 },
+  {
+    id: 10,
+    name: "Төгөлдөр",
+    age: 27,
+    position: "Backend Developer",
+    salary: 2800000,
+  },
+];
+
+console.log("===================== Bituunii bodlogo 1 ===================");
+
+const employeeName = (list) => {
+  for (let i = 0; i < list.length; i++) {
+    console.log("Employee name: ", list[i].name);
+  }
+};
+
+employeeName(employees);
+
+console.log("===================== Bituunii bodlogo 2 ===================");
+
+const employeeAvgAge = (list) => {
+  let avgAge = 0;
+  let ageSum = 0;
+  for (let i = 0; i < list.length; i++) {
+    ageSum = ageSum + list[i].age;
+  }
+  avgAge = ageSum / list.length;
+  console.log("Employee average age: ", avgAge);
+};
+
+employeeAvgAge(employees);
+
+console.log("===================== Bituunii bodlogo 3 ===================");
+
+const employee30plus = (list, nas) => {
+  let findAge = [];
+  let j = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].age >= nas) {
+      findAge[j] = list[i];
+      j++;
+    }
+  }
+  return findAge;
+};
+
+let result = employee30plus(employees, 30);
+console.log(result);
+
+console.log("===================== Bituunii bodlogo 4 ===================");
+const findDev = (list, position) => {
+  let findDev = [];
+  let j = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].position === position) {
+      findDev[j] = list[i];
+      j++;
+    }
+  }
+  return findDev;
+};
+
+let resultDev = findDev(employees, "Developer");
+console.log(resultDev);
+
+console.log("===================== Bituunii bodlogo 5 ===================");
+
+const totalNumOfEmployees = (list) => {
+  return list.length;
+};
+
+let totalEmployeecount = totalNumOfEmployees(employees);
+console.log(totalEmployeecount);
+>>>>>>> Stashed changes
