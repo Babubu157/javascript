89bac068c6b2ea57d34d624348c8963ec122e87d
babubu157 <gboloro@gmail.com>
2025-03-18 02:16:29 +0800

    calc . bolon 0 uildel nemev

--- calc/calc.js
+++ calc/calc.js
@@ -1,82 +1,101 @@
 const numberBtns = document.getElementsByClassName("number");
 const display = document.getElementById("display");
 const btnAdd = document.getElementById("add");
-let operand1 = 0;
-let operand2 = 0;
-let operator = " ";
-let result = 0;
 //const addBtn = document.getElementByClass("add")[0];
 // class -r sobgohod array butsaadag uchir 1 utga butsaaj bgaa ch array helbertei bichne
 const btnSub = document.getElementById("sub");
 const btnMulti = document.getElementById("multi");
 const btnDiv = document.getElementById("divide");
 const btnEqual = document.getElementById("equal");
-// const btnDot = document.getElementById("dot")
+const btnDot = document.getElementById("dot");
+// const btnZero = document.getElementById("zero");
+const btnClear = document.getElementById("clear");
+let operand1 = 0;
+let operand2 = 0;
+let operator = " ";
+let result = 0;
 
-
-for( const btn of numberBtns) {
-    btn.addEventListener("click", () => {
-        console.log("Clicked", btn.innerText);
-        display.innerText += btn.innerText;
-    })
+for (const btn of numberBtns) {
+  btn.addEventListener("click", () => {
+    console.log("Clicked", btn.innerText);
+    if (display.innerText === "0") {
+      display.innerText = btn.innerText;
+    } else {
+      display.innerText += btn.innerText;
+    }
+  });
 }
 
-
 btnAdd.addEventListener("click", () => {
-    console.log("clicked add button");
-    operand1 = display.innerText;
-    operator = "add";
-    display.innerText = "";
+  console.log("clicked add button");
+  operand1 = display.innerText;
+  operator = "add";
+  display.innerText = "";
 });
 
 btnSub.addEventListener("click", () => {
-    console.log("clicked sub button");
-    operand1 = display.innerText;
-    operator = "sub";
-    display.innerText = "";
+  console.log("clicked sub button");
+  operand1 = display.innerText;
+  operator = "sub";
+  display.innerText = "";
 });
 
 btnMulti.addEventListener("click", () => {
-    console.log("clicked sub button");
-    operand1 = display.innerText;
-    operator = "multi";
-    display.innerText = "";
+  console.log("clicked sub button");
+  operand1 = display.innerText;
+  operator = "multi";
+  display.innerText = "";
 });
 
 btnDiv.addEventListener("click", () => {
-    console.log("clicked sub button");
-    operand1 = display.innerText;
-    operator = "divide";
-    display.innerText = "";
+  console.log("clicked sub button");
+  operand1 = display.innerText;
+  operator = "divide";
+  display.innerText = "";
 });
 
-btnEqual.addEventListener("click", 
-    () => { 
-
-        operand2 = display.innerText;
-        console.log(operand1);
-        console.log(operand2);
-    if (operator === "add"){
-    result = parseFloat(operand1) + parseFloat(operand2);
-    console.log(operand1);
-        console.log(operand2);
-    }
-    else if(operator === "sub"){
-    result = parseFloat(operand1) - parseFloat(operand2);
-    console.log(operand1);
-        console.log(operand2);
-    }
-    else if(operator === "multi"){
-    result = parseFloat(operand1) * parseFloat(operand2);  
-    console.log(operand1);
-        console.log(operand2);
-    }
-    else if(operator === "divide"){
-    result = parseFloat(operand1) / parseFloat(operand2);   
-    console.log(operand1);
-        console.log(operand2);
-    }
-    display.innerText = result;
+btnDot.addEventListener("click", () => {
+  if (!display.innerText.includes(".")) {
+    display.innerText += btnDot.innerText;
+  }
 });
 
+btnEqual.addEventListener("click", () => {
+  operand2 = display.innerText;
+  console.log(operand1);
+  console.log(operand2);
+  if (operator === "add") {
+    result = parseFloat(
+      (parseFloat(operand1) + parseFloat(operand2)).toFixed(8)
+    );
+    if (result) console.log(operand1);
+    console.log(operand2);
+  } else if (operator === "sub") {
+    result = parseFloat(
+      (parseFloat(operand1) - parseFloat(operand2)).toFixed(2)
+    );
+    console.log(operand1);
+    console.log(operand2);
+  } else if (operator === "multi") {
+    result = parseFloat(
+      (parseFloat(operand1) * parseFloat(operand2)).toFixed(2)
+    );
+    console.log(operand1);
+    console.log(operand2);
+  } else if (operator === "divide") {
+    result = parseFloat(
+      (parseFloat(operand1) / parseFloat(operand2)).toFixed(2)
+    );
+    console.log(operand1);
+    console.log(operand2);
+  }
 
+  display.innerText = result;
+});
+
+btnClear.addEventListener("click", () => {
+  display.innerText = "";
+  operand1 = 0;
+  operand2 = 0;
+  result = 0;
+});
