0c23387989dea7fdfb5e948bc2c872c24a6297fb
babubu157 <gboloro@gmail.com>
2025-03-17 09:27:43 +0800

    calc

--- js-dom/watch.js
+++ js-dom/watch.js
@@ -1,21 +1,51 @@
-const time = new Date();
-  console.log("Time: ", time.getFullYear());
-  console.log("Time: ", time.getMonth());
-  console.log("Time: ", time.getDate());
-  console.log("Time: ", time.getDay());
-  console.log("Time: ", time.getHours());
-  console.log("Time: ", time.getMinutes());
-  console.log("Time: ", time.getSeconds());
+// const time = new Date();
+// console.log("Time: ", time.getFullYear());
+// console.log("Time: ", time.getMonth());
+// console.log("Time: ", time.getDate());
+// console.log("Time: ", time.getDay());
+// console.log("Time: ", time.getHours());
+// console.log("Time: ", time.getMinutes());
+// console.log("Time: ", time.getSeconds());
 
 let date = document.getElementById("date");
-let times = document.getElementById("time");
-const time1 = new Date();
+let hour = document.getElementById("hour");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+let dateTime = new Date();
 
 const dayWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-const Month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-let 
-const hevleh = () => { {
-    date.innerText = `${ }`
-}
+const month = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
-}
+let thisDayOfWeek = dayWeek[dateTime.getDay()];
+let thisMonth = month[dateTime.getMonth()];
+
+date.innerText = `${thisDayOfWeek}, ${dateTime.getDate()} ${thisMonth} ${dateTime.getFullYear()}`;
+// if (timerId === null) {
+//   timerId = setInterval(() => {
+//     times.innerText = `${time1.getHours()}:${time1.getMinutes()}:${time1.getSeconds()}`;
+//   }, 1000);
+// }
+// const displayTime = () => {
+//   console.log("start");
+//   times.innerText = `${time1.getHours()}:${time1.getMinutes()}:${time1.getSeconds()}`;
+// };
+
+setInterval(() => {
+  hour.innerText = dateTime.getHours();
+  min.innerText = dateTime.getMinutes();
+  sec.innerText = dateTime.getSeconds();
+}, 1000);
